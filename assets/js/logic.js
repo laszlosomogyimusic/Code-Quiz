9cfd136ed42b2d3a35392254a6da70397cd61aa8
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-04-21 20:12:50 +0100

    display question Each choice has correct/wrong dataset

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,11 @@
-var startScreenElement = document.querySelector("#start-screen");
-var endScreenElement = document.querySelector("#end-screen");
+var startScreen = document.querySelector("#start-screen");
+var endScreen = document.querySelector("#end-screen");
 var feedbackElement = document.querySelector("#feedback");
 var timerElement = document.querySelector("#time");
 var startButton = document.querySelector("#start");
-var questionsElement = document.querySelector("#questions");
-
+var questions = document.querySelector("#questions");
+var questionTitle = document.querySelector("#question-title");
+var choices = document.querySelector("#choices");
 
 
 var isWin = false;
@@ -12,23 +13,55 @@ var timerCount = 0;
 
 function startGame() {
   isWin = false;
-  timerCount = 4;
+  timerCount = 3;
   // Prevents start button from being clicked when round is in progress
   startButton.disabled = true;
   startTimer();
-  hideElement(startScreenElement);
+  hideElement(startScreen);
+  displayQuestion();
 }
 
 // The endGame function is called when timer reaches 0
 function endGame() {
   //TBD
   startButton.disabled = false;
-  showElement(startScreenElement);
+  showElement(startScreen);
   showElement(endScreenElement);
 }
 
 function displayQuestion() {
-  showElement(questionsElement);
+  showElement(questions);
+  questionTitle.textContent = "First Question";
+  var ol = document.createElement("ol");
+  var li1 = document.createElement("li");
+  var li2 = document.createElement("li");
+  var li3 = document.createElement("li");
+  var li4 = document.createElement("li");
+
+  var btn1 = document.createElement("button");
+  btn1.dataset.state = "correct";
+  var btn2 = document.createElement("button");
+  btn2.dataset.state = "wrong";
+  var btn3 = document.createElement("button");
+  btn3.dataset.state = "wrong";  
+  var btn4 = document.createElement("button");
+  btn4.dataset.state = "wrong";
+
+  btn1.textContent = "Answer 1";
+  btn2.textContent = "Answer 2";
+  btn3.textContent = "Answer 3";
+  btn4.textContent = "Answer 4";
+
+  li1.appendChild(btn1);
+  li2.appendChild(btn2);
+  li3.appendChild(btn3);
+  li4.appendChild(btn4);
+
+  ol.appendChild(li1);
+  ol.appendChild(li2);
+  ol.appendChild(li3);
+  ol.appendChild(li4);
+  choices.appendChild(ol);
 }
 
 // The setTimer function starts and stops the timer and triggers endGame()
