7848611f85975d1b1c11ef65a22142963434f4e3
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-04-22 22:58:38 +0100

    Multiple questions, evaluate answers, score system The application goes through all the questions defined in the questions.js and evaluate the answers. The scoring/penalty system is also applied.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,64 +6,82 @@ var startButton = document.querySelector("#start");
 var questionsElement = document.querySelector("#questions");
 var questionTitle = document.querySelector("#question-title");
 var choices = document.querySelector("#choices");
+var finalScoreElement = document.querySelector("#final-score");
 
+var isEndGame = false;
 
-var isWin = false;
+//the actual question number (works from the questions array)
+var questionNumber = 0;
+//timer counter in seconds
 var timerCount = 0;
+//deducts 10 seconds from the timer on a wrong answer
+var penaltyOnWrongAnswer = 10;
+
+//the score number. Increments by 1 on a correct answer
+var score = 0;
+
+function initVars() {
+  isEndGame = false;
+  timerCount = 50;
+  questionNumber = 0;
+  score = 0;
+}
 
 function startGame() {
-  isWin = false;
-  timerCount = 3;
+  initVars();
   // Prevents start button from being clicked when round is in progress
   startButton.disabled = true;
-  startTimer();
+  hideElement(feedback);
   hideElement(startScreen);
-  displayQuestion(question2);
+  hideElement(endScreen);
+
+  startTimer();
+  displayQuestion(questions[questionNumber]);
 }
 
 // The endGame function is called when timer reaches 0
 function endGame() {
-  //TBD
+  finalScoreElement.textContent = score;
   startButton.disabled = false;
+  hideElement(questionsElement);
+  clearChoices();
   showElement(startScreen);
   showElement(endScreen);
 }
 
-function buttonClicked(event) {
-  var feedbackText = "";
-  var audio = ""; 
-
-  if(this.dataset.state === answerStates.correct) {
-    feedbackText = "Correct answer";
-    audio = new Audio('assets/sfx/correct.wav');
-  } else {
-    feedbackText = "Wrong answer";
-    audio = new Audio('assets/sfx/incorrect.wav');
-  }
-
-  feedback.textContent = feedbackText;
-  showElement(feedback);
-  audio.play();
-
+function waitMilliseconds(milliseconds) {
+  var wait = setInterval(function() {
+    console.log("wait " + milliseconds + " ms");
+  }, milliseconds);
 }
 
 function generateChoices(selectedQuestion) {
   var ol = document.createElement("ol");
+  ol.setAttribute("id", "choices-list");
 
+  //the answer list starts from 1 (see questions.js)
   for(var i=1; i<=numberOfAnswers; i++) {
     var li = document.createElement("li");
     var btn = document.createElement("button");
-    //dynamically create the variable name selectedQuestion.answer1Text, selectedQuestion.answer2Text, etc...
+    //dynamically create the variable name: selectedQuestion.answer1Text, selectedQuestion.answer2Text, etc...
     btn.textContent = eval("selectedQuestion.answer"+i+"Text");
-    //dynamically create the variable name selectedQuestion.answer1State, selectedQuestion.answer2State, etc...
+    //dynamically create the variable name: selectedQuestion.answer1State, selectedQuestion.answer2State, etc...
     btn.dataset.state = eval("selectedQuestion.answer"+i+"State");
-    btn.addEventListener("click", buttonClicked);
+    
+    //this event evaluates the answer and displays the next question or end the game
+    btn.addEventListener("click", evaluateAnswer);
 
     li.appendChild(btn);
     ol.appendChild(li);
   }
 
   choices.appendChild(ol);
+}
+
+//this function clears the choices list
+function clearChoices() {
+  var choicesList = document.querySelector("#choices-list");
+  choices.removeChild(choicesList);
 }
 
 //the displayQuestion shows the question and choices
@@ -73,13 +91,58 @@ function displayQuestion(selectedQuestion) {
   generateChoices(selectedQuestion);
 }
 
+//this function could either finish the quiz when reach the last question
+//or loads the next question
+function prepareNextQuestion() {
+  questionNumber++;
+
+  hideElement(feedback);
+  hideElement(questionsElement);
+  clearChoices();  
+  if(questionNumber === questions.length) {
+    isEndGame = true;
+  } else {
+    displayQuestion(questions[questionNumber]);
+  }
+}
+
+//this is a button click event, which evaluates the actual answer
+//and loads the next question
+function evaluateAnswer(event) {
+  var feedbackText = "";
+  var audio = ""; 
+
+  if(this.dataset.state === answerStates.correct) {
+    score++;
+    feedbackText = "Correct answer";
+    audio = new Audio('assets/sfx/correct.wav');
+  } else {
+    timerCount = timerCount - penaltyOnWrongAnswer;
+    if(timerCount <= 0) {
+      isEndGame = true;
+      //this is a trick to display 0 timercount at the end screen, because
+      //the main timer keeps going for one more round
+      timerCount = 1;
+    }
+    feedbackText = "Wrong answer";
+    audio = new Audio('assets/sfx/incorrect.wav');
+  }
+
+  feedback.textContent = feedbackText;
+  showElement(feedback);
+  audio.play();
+  waitMilliseconds(2000);
+  prepareNextQuestion();
+}
+
+
 // The setTimer function starts and stops the timer and triggers endGame()
 function startTimer() {
   timer = setInterval(function() {
     timerCount--;
     timerElement.textContent = timerCount;
 
-    if(timerCount === 0) {
+    if(isEndGame || timerCount === 0) {
       clearInterval(timer);
       endGame();
     }
@@ -96,7 +159,6 @@ function hideElement(element) {
 function showElement(element) {
   element.className = element.className.replace( /(?:^|\s)hide(?!\S)/g , '' );
 }
-
 
 // Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startGame);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,10 +1,18 @@
+//this declares the number of answer per question
 var numberOfAnswers = 4;
 
+//to avoid hardcoded values when checking the answer states this variable is handy
 var answerStates = {
   correct: "correct",
   wrong: "wrong"
 };
 
+/***************  !!!! IMPORTANT !!! ***************************
+The questions array is at the end of the file, it must be 
+maintained when a new question is added or an existing is removed
+****************************************************************/
+
+/******* Questions **********/
 var question1 = {
   question: "Question 1",
   answer1Text: "Answer 1 - wrong",
@@ -14,11 +22,23 @@ var question1 = {
   answer3Text: "Answer 3 - correct",
   answer3State: answerStates.correct,
   answer4Text: "Answer 4 - wrong",
-  answer4State: answerStates.wrong,
+  answer4State: answerStates.wrong
 };
 
 var question2 = {
   question: "Question 2",
+  answer1Text: "Answer 1 - wrong",
+  answer1State: answerStates.wrong,
+  answer2Text: "Answer 2 - correct",
+  answer2State: answerStates.correct,
+  answer3Text: "Answer 3 - wrong",
+  answer3State: answerStates.wrong,
+  answer4Text: "Answer 4 - wrong",
+  answer4State: answerStates.wrong
+};
+
+var question3 = {
+  question: "Question 3",
   answer1Text: "Answer 1 - correct",
   answer1State: answerStates.correct,
   answer2Text: "Answer 2 - wrong",
@@ -26,7 +46,33 @@ var question2 = {
   answer3Text: "Answer 3 - wrong",
   answer3State: answerStates.wrong,
   answer4Text: "Answer 4 - wrong",
-  answer4State: answerStates.wrong,
+  answer4State: answerStates.wrong
 };
 
-var questions = [question1, question2];
+var question4 = {
+  question: "Question 4",
+  answer1Text: "Answer 1 - wrong",
+  answer1State: answerStates.wrong,
+  answer2Text: "Answer 2 - wrong",
+  answer2State: answerStates.wrong,
+  answer3Text: "Answer 3 - wrong",
+  answer3State: answerStates.wrong,
+  answer4Text: "Answer 4 - correct",
+  answer4State: answerStates.correct
+};
+
+var question5 = {
+  question: "Question 5",
+  answer1Text: "Answer 1 - wrong",
+  answer1State: answerStates.wrong,
+  answer2Text: "Answer 2 - wrong",
+  answer2State: answerStates.wrong,
+  answer3Text: "Answer 3 - correct",
+  answer3State: answerStates.correct,
+  answer4Text: "Answer 4 - wrong",
+  answer4State: answerStates.wrong
+};
+/******* END Questions **********/
+
+//this is an important array, the application goes through this one
+var questions = [question1, question2, question3, question4, question5];
