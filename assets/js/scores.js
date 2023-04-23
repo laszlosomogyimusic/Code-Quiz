4cae5bff4ebf381a2a67c2e752492706f22c909f
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-04-24 00:18:19 +0100

    High Score working The high scores are stored in the local storage, displayed on the screen and also can be cleared.

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,27 +3,28 @@ var clearButton = document.querySelector("#clear");
 
 function displayHighScores() {
   var savedScores = localStorage.getItem(localStorageElements.score); // get the score, or the initial value if empty
-  var highScores = [...JSON.parse(savedScores), scoreStructure];
   
-  for(var i=0; i<highScores.length; i++) {
-    var li = document.createElement("li");
-    console.log(highScores[i]);
-    var aa = highScores[i].initial;
-
-
-
-    console.log(highScores);
-    //var score = JSON.parse();
-
-    //li.textContent = score.initial;
-    //highScoresElement.appendChild(li);
-
+  if (savedScores !== null) {
+    var highScores = [...JSON.parse(savedScores), scoreStructure];
+    
+    for(var i=0; i<highScores.length; i++) {
+      var li = document.createElement("li");
+      console.log(highScores[i]);
+      var aa = highScores[i].initial;
+      var bb = highScores[i].score;
+      li.textContent = highScores[i].initial + " - " + highScores[i].score;
+      highScoresElement.appendChild(li);
+    }
   }
-  
 }
 
 function clearHighScores() {
+  localStorage.clear();
 
+  while (highScoresElement.firstChild) {
+    highScoresElement.removeChild(highScoresElement.firstChild);
+  }
+  displayHighScores();
 }
 
 clearButton.addEventListener("click", clearHighScores);
